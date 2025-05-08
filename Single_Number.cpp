695e1e7fa29b4798626165cbdea19a528141e263
devsamin <abdullahalsamin79@gmail.com>
2025-05-08 14:48:29 -0700

    optimize code

--- Single_Number.cpp
+++ Single_Number.cpp
@@ -1,18 +1,26 @@
 class Solution {
     public:
         int singleNumber(vector<int>& nums) {
-            map<int,int> freqmap;
+            // map<int,int> freqmap;
     
-            for(int num : nums){
-                freqmap[num]++;
-            }
+            // for(int num : nums){
+            //     freqmap[num]++;
+            // }
     
-            for(auto const&[key, val] : freqmap){
-                if(val == 1){
-                    return key;
-                }
+            // for(auto const&[key, val] : freqmap){
+            //     if(val == 1){
+            //         return key;
+            //     }
+            // }
+            // return -1;
+    
+    
+            // 0(n).
+            int ans = 0;
+            for(int val : nums){
+                ans = ans ^ val;
             }
-            return -1;
+            return ans;
         }
     
     };
