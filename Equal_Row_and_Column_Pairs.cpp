30745fa8fa11351af02e25658d83dc7175a3ebb2
devsamin <abdullahalsamin79@gmail.com>
2025-07-13 11:29:10 -0700

    2352. Equal Row and Column Pairs Optimize Appoche o(n^2)

--- Equal_Row_and_Column_Pairs.cpp
+++ Equal_Row_and_Column_Pairs.cpp
@@ -1,25 +1,47 @@
 class Solution {
 public:
     int equalPairs(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
+        // Brute Force 0(n^3)
 
-        int ans = 0;
+        // int n = grid.size();
+        // int m = grid[0].size();
+
+        // int ans = 0;
+
+        // for(int r=0;r<n;r++){
+        //     for(int c=0;c<m;c++){
+        //         bool isequal = true;
+        //         for(int i=0;i<n;i++){
+        //             if(grid[r][i] != grid[i][c]){
+        //                 isequal = false;
+        //                 break;
+        //             }
+        //         }
+        //         if(isequal){
+        //             ans++;
+        //         }
+        //     }
+        // }
+        // return ans;
+
+        // Optimal Approche o(n^2)
+
+        int n = grid.size();
+        int count = 0;
+        map<vector<int>, int> mp; // row stor
 
         for(int r=0;r<n;r++){
-            for(int c=0;c<m;c++){
-                bool isequal = true;
-                for(int i=0;i<n;i++){
-                    if(grid[r][i] != grid[i][c]){
-                        isequal = false;
-                        break;
-                    }
-                }
-                if(isequal){
-                    ans++;
-                }
-            }
+            mp[grid[r]]++;
         }
-        return ans;
+
+        for(int c=0;c<n;c++){
+            vector<int> tmp; // col stor
+
+            for(int i=0;i<n;i++){
+                tmp.push_back(grid[i][c]);
+            }
+            count+=mp[tmp];
+        }
+        return count;
     }
 };
